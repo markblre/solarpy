53108e3dee2264643727f36e284654e7f9f688e5
Mark Ballereau <mark.b@live.fr>
2019-03-28 18:33:07 +0100

    first commit

---
+++ simulation.py
@@ -0,0 +1,463 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from OpenGL.GLE import *
+
+from PIL import Image
+import numpy
+
+import pygame
+from pygame.locals import *
+from numpy import array
+
+from skyfield.api import load
+import time
+
+ts = load.timescale()
+
+second = 0
+realTim = False
+
+def goSimulation(SCREEN_SIZE, showSun, showMercury, showVenus, showEarth,
+                 showMars, showJupiter, showSaturn, showUranus, showNeptune,
+                 years, month, day, hour, minute, seconde, realTime, speed):
+
+    global second
+    global realTim
+    second = seconde
+    realTim = realTime
+    
+    planets = load('de421.bsp')
+
+    sun = planets['sun']
+    mercury = planets['mercury barycenter']
+    venus = planets['venus barycenter']
+    earth = planets['earth barycenter']
+    mars = planets['mars barycenter']
+    jupiter = planets['jupiter barycenter']
+    saturn = planets['saturn barycenter']
+    uranus = planets['uranus barycenter']
+    neptune = planets['neptune barycenter']
+    
+
+    pointAxes = (
+        (0, 0, 0),
+        (100, 0, 0),
+        (0, 100, 0),
+        (0, 0, 100)
+        )
+
+    edges = (
+        (0,1),
+        (0,2),
+        (0,3)
+        )
+
+    def Axes():
+        glBegin(GL_LINES)
+        for edge in edges:
+            for vertex in edge:
+                glVertex3fv(pointAxes[vertex])
+        glEnd()
+        
+    def resize(width, height):
+        
+        glViewport(0, 0, width, height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, float(width)/height, .1, 1000.)
+        gluLookAt(0, 0, 100, # endroit où la camera est
+                  0, 0, 0, # endroit où la camera regarde
+                  0, 1, 0) # 
+        glMatrixMode(GL_MODELVIEW)
+            
+        glLoadIdentity()
+
+
+    def init():
+        
+        glEnable(GL_DEPTH_TEST)
+        
+        glShadeModel(GL_FLAT)
+        glClearColor(0, 0, 0, 0.0)
+
+        glEnable(GL_COLOR_MATERIAL)
+        
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)        
+        glLight(GL_LIGHT0, GL_POSITION,  (0, 0, 0, 1))
+
+    def read_texture(filename):
+        img = Image.open(filename)
+        img_data = numpy.array(list(img.getdata()), numpy.int8)
+        texture_id = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture_id)
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0,
+                     GL_RGB, GL_UNSIGNED_BYTE, img_data)
+        return texture_id
+
+    class Ball(object):
+        
+        def __init__(self, position, texture, size):
+            
+            self.position = array (position)
+            self.texture = texture
+            self.size = size
+            self.display_list = None
+            self.trace = []
+
+        def render(self):
+            glPushMatrix()
+            global sunTexture
+            global mercuryTexture
+            global venusTexture
+            global earthTexture
+            global marsTexture
+            global jupiterTexture
+            global saturnTexture
+            global uranusTexture
+            global neptuneTexture
+            if self.texture == "sunTexture":
+                texture_id = sunTexture
+            elif self.texture == "mercuryTexture":
+                texture_id = mercuryTexture
+            elif self.texture == "venusTexture":
+                texture_id = venusTexture
+            elif self.texture == "earthTexture":
+                texture_id = earthTexture
+            elif self.texture == "marsTexture":
+                texture_id = marsTexture
+            elif self.texture == "jupiterTexture":
+                texture_id = jupiterTexture
+            elif self.texture == "saturnTexture":
+                texture_id = saturnTexture
+            elif self.texture == "uranusTexture":
+                texture_id = uranusTexture
+            elif self.texture == "neptuneTexture":
+                texture_id = neptuneTexture
+                
+            glEnable(GL_TEXTURE_2D)
+            glBindTexture(GL_TEXTURE_2D, texture_id)
+            glEnable(GL_TEXTURE_GEN_S)
+            glEnable(GL_TEXTURE_GEN_T)
+            glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP)
+            glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP)
+            glTranslatef(self.position[0],self.position[1],self.position[2])
+            glutSolidSphere(self.size,50,50)
+            glDisable(GL_TEXTURE_2D)
+            glPopMatrix()
+            glutSwapBuffers()
+
+    def resetRotate():
+        for i in saveChangeRotate:
+            if i == "a":
+                glRotatef(-1, 1, 0, 0)
+            elif i == "z":
+                glRotatef(1, 1, 0, 0)
+            elif i == "e":
+                glRotatef(-1, 0, 1, 0)
+            elif i == "r":
+                glRotatef(1, 0, 1, 0)
+            elif i == "t":
+                glRotatef(-1, 0, 0, 1)
+            elif i == "y":
+                glRotatef(1, 0, 0, 1)
+
+    def backRotate():
+        for i in saveChangeRotate:
+            if i == "a":
+                glRotatef(-1, 1, 0, 0)
+            elif i == "z":
+                glRotatef(1, 1, 0, 0)
+            elif i == "e":
+                glRotatef(-1, 0, 1, 0)
+            elif i == "r":
+                glRotatef(1, 0, 1, 0)
+            elif i == "t":
+                glRotatef(-1, 0, 0, 1)
+            elif i == "y":
+                glRotatef(1, 0, 0, 1)
+
+    def run():
+        pygame.init()
+        screen = pygame.display.set_mode(SCREEN_SIZE, HWSURFACE|OPENGL|DOUBLEBUF|FULLSCREEN)
+        pygame.display.set_caption("Solarpy")
+        
+        resize(*SCREEN_SIZE)
+        init()
+
+        print("10%")
+
+        global sunTexture
+        global mercuryTexture
+        global venusTexture
+        global earthTexture
+        global marsTexture
+        global jupiterTexture
+        global saturnTexture
+        global uranusTexture
+        global neptuneTexture
+        sunTexture = read_texture('sun.jpg')
+        print("20%")
+        mercuryTexture = read_texture('mercury.jpg')
+        print("30%")
+        venusTexture = read_texture('venus.jpg')
+        print("40%")
+        earthTexture = read_texture('earth.jpg')
+        print("50%")
+        marsTexture = read_texture('mars.jpg')
+        print("60%")
+        jupiterTexture = read_texture('jupiter.jpg')
+        print("70%")
+        saturnTexture = read_texture('saturn.jpg')
+        print("80%")
+        uranusTexture = read_texture('uranus.jpg')
+        print("90%")
+        neptuneTexture = read_texture('neptune.jpg')
+        print("99%")
+             
+        glMaterial(GL_FRONT, GL_AMBIENT, (0.1, 0.1, 0.1, 1.0))    
+        glMaterial(GL_FRONT, GL_DIFFUSE, (1.0, 1.0, 1.0, 1.0))
+
+        #tailles planètes
+        sizeBall = 1
+        sunSize = 2
+        mercurySize = 0.035
+        venusSize = 0.087
+        earthSize = 0.0391
+        marsSize = 0.0487
+        jupiterSize = 1
+        saturnSize = 0.837
+        uranusSize = 0.364
+        neptuneSize = 0.354
+
+        #déclaration des planetes
+        Sun = Ball([0,0,0], "sunTexture", sunSize)
+        Mercury = Ball([0,0,0], "mercuryTexture", mercurySize * sizeBall)
+        Venus = Ball([0,0,0], "venusTexture", venusSize * sizeBall)
+        Earth = Ball([0,0,0], "earthTexture", earthSize * sizeBall)
+        Mars = Ball([0,0,0], "marsTexture", marsSize * sizeBall)
+        Jupiter = Ball([0,0,0], "jupiterTexture", jupiterSize * sizeBall)
+        Saturn = Ball([0,0,0], "saturnTexture", saturnSize * sizeBall)
+        Uranus = Ball([0,0,0], "uranusTexture", uranusSize * sizeBall)
+        Neptune = Ball([0,0,0], "neptuneTexture", neptuneSize * sizeBall)
+
+        #for zoom
+        saveChangeRotate = ""
+        saveChangeRotateReturn = ""
+        #a  X+1
+        #z  X-1
+        #e  Y+1
+        #r  Y-1
+        #t  Z+1
+        #y  Z-1
+
+        pygame.key.set_repeat(1,20)
+        
+        while True:
+            for event in pygame.event.get():          
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+                if event.type == KEYDOWN and event.key == K_p:
+                    pygame.quit()
+                    quit()
+                
+                if event.type == KEYDOWN and event.key == K_x:
+                    for i in saveChangeRotate:
+                        if i == "a":
+                            glRotatef(-1, 1, 0, 0)
+                        elif i == "z":
+                            glRotatef(1, 1, 0, 0)
+                        elif i == "e":
+                            glRotatef(-1, 0, 1, 0)
+                        elif i == "r":
+                            glRotatef(1, 0, 1, 0)
+                        elif i == "t":
+                            glRotatef(-1, 0, 0, 1)
+                        elif i == "y":
+                            glRotatef(1, 0, 0, 1)
+                    glTranslatef(0,0,0.5)
+                    sunSize -= 0.01
+                    sizeBall -= 0.001
+                    for i in saveChangeRotateReturn:
+                        if i == "a":
+                            glRotatef(1, 1, 0, 0)
+                        elif i == "z":
+                            glRotatef(-1, 1, 0, 0)
+                        elif i == "e":
+                            glRotatef(1, 0, 1, 0)
+                        elif i == "r":
+                            glRotatef(-1, 0, 1, 0)
+                        elif i == "t":
+                            glRotatef(1, 0, 0, 1)
+                        elif i == "y":
+                            glRotatef(-1, 0, 0, 1)
+                            
+                if event.type == KEYDOWN and event.key == K_w:
+                    for i in saveChangeRotate:
+                        if i == "a":
+                            glRotatef(-1, 1, 0, 0)
+                        elif i == "z":
+                            glRotatef(1, 1, 0, 0)
+                        elif i == "e":
+                            glRotatef(-1, 0, 1, 0)
+                        elif i == "r":
+                            glRotatef(1, 0, 1, 0)
+                        elif i == "t":
+                            glRotatef(-1, 0, 0, 1)
+                        elif i == "y":
+                            glRotatef(1, 0, 0, 1)
+                    glTranslatef(0,0,-0.5)
+                    sunSize += 0.01
+                    sizeBall += 0.001
+                    for i in saveChangeRotateReturn:
+                        if i == "a":
+                            glRotatef(1, 1, 0, 0)
+                        elif i == "z":
+                            glRotatef(-1, 1, 0, 0)
+                        elif i == "e":
+                            glRotatef(1, 0, 1, 0)
+                        elif i == "r":
+                            glRotatef(-1, 0, 1, 0)
+                        elif i == "t":
+                            glRotatef(1, 0, 0, 1)
+                        elif i == "y":
+                            glRotatef(-1, 0, 0, 1)
+
+
+                if event.type == KEYDOWN and event.key == K_r:
+                    glRotatef(1, 1, 0, 0) #(degres, x, y, z)
+                    saveChangeRotate = "a" + saveChangeRotate
+                    saveChangeRotateReturn += "a"
+                if event.type == KEYDOWN and event.key == K_e:
+                    glRotatef(-1, 1, 0, 0)
+                    saveChangeRotate = "z" + saveChangeRotate
+                    saveChangeRotateReturn += "z"
+
+                if event.type == KEYDOWN and event.key == K_f:
+                    glRotatef(1, 0, 1, 0)
+                    saveChangeRotate = "e" + saveChangeRotate
+                    saveChangeRotateReturn += "e"
+                if event.type == KEYDOWN and event.key == K_d:
+                    glRotatef(-1, 0, 1, 0)
+                    saveChangeRotate = "r" + saveChangeRotate
+                    saveChangeRotateReturn += "r"
+
+                if event.type == KEYDOWN and event.key == K_v:
+                    glRotatef(1, 0, 0, 1)
+                    saveChangeRotate = "t" + saveChangeRotate
+                    saveChangeRotateReturn += "t"
+                if event.type == KEYDOWN and event.key == K_c:
+                    glRotatef(-1, 0, 0, 1)
+                    saveChangeRotate = "y" + saveChangeRotate
+                    saveChangeRotateReturn += "y"
+                    
+
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glLight(GL_LIGHT0, GL_POSITION,  (0, 0, 0, 1))
+
+            Sun = Ball([0,0,0], "sunTexture", sunSize)
+            Axes()
+            #Mise a jour des données
+            global second
+            global realTim
+
+            if not realTim:
+                t = ts.utc(years, month, day, hour, minute, second)
+                second += (speed/100)
+                
+            if realTim:
+                t = ts.now()
+            
+            if(showMercury == 1):
+                posMercury = sun.at(t).observe(mercury).position.au
+                #print(posMercury)
+                MercuryX = posMercury[0] * 1.3
+                MercuryY = posMercury[1] * 1.3
+                MercuryZ = posMercury[2] * 1.3
+                Mercury = Ball([MercuryX,MercuryY,MercuryZ], "mercuryTexture", mercurySize * sizeBall)
+                Mercury.render()
+
+            if(showVenus == 1):
+                posVenus = sun.at(t).observe(venus).position.au
+                #print(posVenus)
+                VenusX = posVenus[0] * 1.3
+                VenusY = posVenus[1] * 1.3
+                VenusZ = posVenus[2] * 1.3
+                Venus = Ball([VenusX,VenusY,VenusZ], "venusTexture", venusSize * sizeBall)
+                Venus.render()
+
+            if(showEarth == 1):
+                posEarth = sun.at(t).observe(earth).position.au
+                #print(posEarth)
+                EarthX = posEarth[0] * 1.3
+                EarthY = posEarth[1] * 1.3
+                EarthZ = posEarth[2] * 1.3
+                Earth = Ball([EarthX,EarthY,EarthZ], "earthTexture", earthSize * sizeBall)
+                Earth.render()
+
+            if(showMars == 1):
+                posMars = sun.at(t).observe(mars).position.au
+                #print(posMars)
+                MarsX = posMars[0] * 1.3
+                MarsY = posMars[1] * 1.3
+                MarsZ = posMars[2] * 1.3
+                Mars = Ball([MarsX,MarsY,MarsZ], "marsTexture", marsSize * sizeBall)
+                Mars.render()
+
+            if(showJupiter == 1):
+                posJupiter = sun.at(t).observe(jupiter).position.au
+                #print(posJupiter)
+                JupiterX = posJupiter[0] * 1.3
+                JupiterY = posJupiter[1] * 1.3
+                JupiterZ = posJupiter[2] * 1.3
+                Jupiter = Ball([JupiterX,JupiterY,JupiterZ], "jupiterTexture", jupiterSize * sizeBall)
+                Jupiter.render()
+
+            if(showSaturn == 1):
+                posSaturn = sun.at(t).observe(saturn).position.au
+                #print(posSaturn)
+                SaturnX = posSaturn[0] * 1.3
+                SaturnY = posSaturn[1] * 1.3
+                SaturnZ = posSaturn[2] * 1.3
+                Saturn = Ball([SaturnX,SaturnY,SaturnZ], "saturnTexture", saturnSize * sizeBall)
+                Saturn.render()
+
+            if(showUranus == 1):
+                posUranus = sun.at(t).observe(uranus).position.au
+                #print(posUranus)
+                UranusX = posUranus[0] * 1.3
+                UranusY = posUranus[1] * 1.3
+                UranusZ = posUranus[2] * 1.3
+                Uranus = Ball([UranusX,UranusY,UranusZ], "uranusTexture", uranusSize * sizeBall)
+                Uranus.render()
+
+            if(showNeptune == 1):
+                posNeptune = sun.at(t).observe(neptune).position.au
+                #print(posNeptune)
+                NeptuneX = posNeptune[0] * 1.3
+                NeptuneY = posNeptune[1] * 1.3
+                NeptuneZ = posNeptune[2] * 1.3
+                Neptune = Ball([NeptuneX,NeptuneY,NeptuneZ], "neptuneTexture", neptuneSize * sizeBall)
+                Neptune.render()
+
+            if(showSun == 1):
+                Sun.render()
+            
+            # Afficher
+            pygame.display.flip()
+            pygame.time.wait(10)
+            time.sleep(0.01)
+    
+    run()
---
+++ solarpy.py
@@ -0,0 +1,327 @@
+SCREEN_SIZE = (2880, 1800)
+#SCREEN_SIZE = (1920, 1200)
+#SCREEN_SIZE = (1680, 1050)
+from simulation import *
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter import filedialog
+    
+#afficher les planetes ?
+showSun = 0
+showMercury = 0
+showVenus = 0
+showEarth = 0
+showMars = 0
+showJupiter = 0
+showSaturn = 0
+showUranus = 0
+showNeptune = 0
+
+#date
+years = 1979
+month = 3
+day = 17
+hour = 22
+minute = 45
+second = 0
+
+#vitesse
+speed = 1
+
+def incrementerVar():
+    global showSun
+    global showMercury
+    global showVenus
+    global showEarth
+    global showMars
+    global showJupiter
+    global showSaturn
+    global showUranus
+    global showNeptune
+    global years
+    global month
+    global day
+    global hour 
+    global minute
+    global second
+    global speed
+    
+    showSun = varShowSun.get()
+    showMercury = varShowMercury.get()
+    showVenus = varShowVenus.get()
+    showEarth = varShowEarth.get()
+    showMars = varShowMars.get()
+    showJupiter = varShowJupiter.get()
+    showSaturn = varShowSaturn.get()
+    showUranus = varShowUranus.get()
+    showNeptune = varShowNeptune.get()
+
+    years = int(sAnnee.get())
+    month = int(sMois.get())
+    day = int(sJour.get())
+    hour = int(sHeure.get())
+    minute = int(sMinute.get())
+    second = int(sSeconde.get())
+
+    speed = float(sVitesse.get())
+    
+def lauchSimulation():
+    incrementerVar()
+    
+    realTime = False
+    
+    goSimulation(SCREEN_SIZE, showSun, showMercury, showVenus, showEarth, showMars,
+                 showJupiter, showSaturn, showUranus, showNeptune,
+                 years, month, day, hour, minute, second, realTime, speed)
+
+def lauchSimulationInRealTime():
+    incrementerVar()
+
+    realTime = True
+    
+    goSimulation(SCREEN_SIZE, showSun, showMercury, showVenus, showEarth, showMars,
+                 showJupiter, showSaturn, showUranus, showNeptune,
+                 years, month, day, hour, minute, second, realTime, speed)
+
+#tkinter
+fenetre = Tk()
+fenetre.filename = ""
+
+def saveFile(ev=None):
+    incrementerVar()
+    
+    if fenetre.filename == "":
+        saveFileAs()
+    else:
+        fichier = open(fenetre.filename, "w")
+        fichier.write(str(showSun) + "\n" + str(showMercury) + "\n" + str(showVenus) + "\n" + str(showEarth) + "\n" + str(showMars) + "\n" + str(showJupiter)
+                      + "\n" + str(showSaturn) + "\n" + str(showUranus) + "\n" + str(showNeptune) + "\n" + str(years) + "\n" + str(month) + "\n" + str(day)
+                      + "\n" + str(hour) + "\n" + str(minute) + "\n" + str(second) + "\n" + str(speed))
+        fichier.close()
+
+def saveFileAs(ev=None):
+    incrementerVar()
+    fenetre.filename =  filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+    fichier = open(fenetre.filename, "w")
+    fichier.write(str(showSun) + "\n" + str(showMercury) + "\n" + str(showVenus) + "\n" + str(showEarth) + "\n" + str(showMars) + "\n" + str(showJupiter)
+                  + "\n" + str(showSaturn) + "\n" + str(showUranus) + "\n" + str(showNeptune) + "\n" + str(years) + "\n" + str(month) + "\n" + str(day)
+                  + "\n" + str(hour) + "\n" + str(minute) + "\n" + str(second) + "\n" + str(speed))
+    fichier.close()
+
+def openFile(ev=None):
+    fenetre.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+    fichier = open(fenetre.filename, "r")
+    fichierSplit = str(fichier.read()).split()
+    
+    if fichierSplit[0] == "0":
+        boutonShowSun.deselect()
+    else:
+        boutonShowSun.select()
+
+    if fichierSplit[1] == "0":
+        boutonShowMercury.deselect()
+    else:
+        boutonShowMercury.select()
+    fichier.close()
+
+    if fichierSplit[2] == "0":
+        boutonShowVenus.deselect()
+    else:
+        boutonShowVenus.select()
+    fichier.close()
+
+    if fichierSplit[3] == "0":
+        boutonShowEarth.deselect()
+    else:
+        boutonShowEarth.select()
+    fichier.close()
+
+    if fichierSplit[4] == "0":
+        boutonShowMars.deselect()
+    else:
+        boutonShowMars.select()
+    fichier.close()
+
+    if fichierSplit[5] == "0":
+        boutonShowJupiter.deselect()
+    else:
+        boutonShowJupiter.select()
+    fichier.close()
+
+    if fichierSplit[6] == "0":
+        boutonShowSaturn.deselect()
+    else:
+        boutonShowSaturn.select()
+    fichier.close()
+
+    if fichierSplit[7] == "0":
+        boutonShowUranus.deselect()
+    else:
+        boutonShowUranus.select()
+    fichier.close()
+
+    if fichierSplit[8] == "0":
+        boutonShowNeptune.deselect()
+    else:
+        boutonShowNeptune.select()
+
+    varsAnnee.set(fichierSplit[9])
+    varsMois.set(fichierSplit[10])
+    varsJour.set(fichierSplit[11])
+    varsHeure.set(fichierSplit[12])
+    varsMinute.set(fichierSplit[13])
+    varsSeconde.set(fichierSplit[14])
+
+    varsVitesse.set(fichierSplit[15])
+
+    fichier.close()
+
+def reset():
+    boutonShowSun.deselect()
+    boutonShowMercury.deselect()
+    boutonShowVenus.deselect()
+    boutonShowEarth.deselect()
+    boutonShowMars.deselect()
+    boutonShowJupiter.deselect()
+    boutonShowSaturn.deselect()
+    boutonShowUranus.deselect()
+    boutonShowNeptune.deselect()
+    varsAnnee.set("1970")
+    varsMois.set("1")
+    varsJour.set("1")
+    varsHeure.set("0")
+    varsMinute.set("0")
+    varsSeconde.set("0")
+    varsVitesse.set("1.0")
+
+selectDeselect = False
+def SelectDeselectAll():
+    global selectDeselect
+    selectDeselect = not selectDeselect
+    
+    if selectDeselect:
+        boutonShowSun.select()
+        boutonShowMercury.select()
+        boutonShowVenus.select()
+        boutonShowEarth.select()
+        boutonShowMars.select()
+        boutonShowJupiter.select()
+        boutonShowSaturn.select()
+        boutonShowUranus.select()
+        boutonShowNeptune.select()
+    else:
+        boutonShowSun.deselect()
+        boutonShowMercury.deselect()
+        boutonShowVenus.deselect()
+        boutonShowEarth.deselect()
+        boutonShowMars.deselect()
+        boutonShowJupiter.deselect()
+        boutonShowSaturn.deselect()
+        boutonShowUranus.deselect()
+        boutonShowNeptune.deselect()
+        
+def quitte():
+    if askyesno('Quitter', 'Êtes-vous sûr de vouloir quitter?'):
+        fenetre.destroy()
+
+#affichage
+#barre de menu
+menubar = Menu(fenetre)
+
+menu1 = Menu(menubar, tearoff=0)
+menu1.add_command(label="Ouvrir...             Ctrl+o", command=openFile)
+fenetre.bind_all("<Command-o>", openFile)
+menu1.add_command(label="Enregistrer           Ctrl+s", command=saveFile)
+fenetre.bind_all("<Command-s>", saveFile)
+menu1.add_command(label="Enregistrer sous...   Ctrl+Shift+s", command=saveFileAs)
+fenetre.bind_all("<Command-S>", saveFileAs)
+menu1.add_separator()
+menu1.add_command(label="Réinitialiser", command=reset)
+menu1.add_separator()
+menu1.add_command(label="Quitter", command=quitte)
+menubar.add_cascade(label="Fichier", menu=menu1)
+
+menu2 = Menu(menubar, tearoff=0)
+menu2.add_command(label="A propos") #ouvrir une page de crédit
+menubar.add_cascade(label="Aide", menu=menu2)
+
+fenetre.config(menu=menubar)
+
+#show planets
+varShowSun = IntVar()
+varShowMercury = IntVar()
+varShowVenus = IntVar()
+varShowEarth = IntVar()
+varShowMars = IntVar()
+varShowJupiter = IntVar()
+varShowSaturn = IntVar()
+varShowUranus = IntVar()
+varShowNeptune = IntVar()
+varsAnnee = StringVar()
+varsMois = StringVar()
+varsJour = StringVar()
+varsHeure = StringVar()
+varsMinute = StringVar()
+varsSeconde = StringVar()
+varsVitesse = StringVar()
+
+boutonSelectAll = Button(fenetre, text="Select/Deselect all", command=SelectDeselectAll)
+boutonSelectAll.pack()
+boutonShowSun = Checkbutton(fenetre, text="ShowSun", variable=varShowSun)
+boutonShowSun.pack()
+boutonShowMercury = Checkbutton(fenetre, text="ShowMercury", variable=varShowMercury)
+boutonShowMercury.pack()
+boutonShowVenus = Checkbutton(fenetre, text="ShowVenus", variable=varShowVenus)
+boutonShowVenus.pack()
+boutonShowEarth = Checkbutton(fenetre, text="ShowEarth", variable=varShowEarth)
+boutonShowEarth.pack()
+boutonShowMars = Checkbutton(fenetre, text="ShowMars", variable=varShowMars)
+boutonShowMars.pack()
+boutonShowJupiter = Checkbutton(fenetre, text="ShowJupiter", variable=varShowJupiter)
+boutonShowJupiter.pack()
+boutonShowSaturn = Checkbutton(fenetre, text="ShowSaturn", variable=varShowSaturn)
+boutonShowSaturn.pack()
+boutonShowUranus = Checkbutton(fenetre, text="ShowUranus", variable=varShowUranus)
+boutonShowUranus.pack()
+boutonShowNeptune = Checkbutton(fenetre, text="ShowNeptune", variable=varShowNeptune)
+boutonShowNeptune.pack()
+
+#date
+labelAnnee = Label(fenetre, text="Année")
+labelAnnee.pack()
+sAnnee = Spinbox(fenetre, from_=1970, to=10000, textvariable=varsAnnee)
+sAnnee.pack()
+labelMois = Label(fenetre, text="Mois")
+labelMois.pack()
+sMois = Spinbox(fenetre, from_=1, to=12, textvariable=varsMois)
+sMois.pack()
+labelJour = Label(fenetre, text="Jour")
+labelJour.pack()
+sJour = Spinbox(fenetre, from_=1, to=31, textvariable=varsJour)
+sJour.pack()
+labelHeure = Label(fenetre, text="Heure")
+labelHeure.pack()
+sHeure = Spinbox(fenetre, from_=0, to=23, textvariable=varsHeure)
+sHeure.pack()
+labelMinute = Label(fenetre, text="Minute")
+labelMinute.pack()
+sMinute = Spinbox(fenetre, from_=0, to=60, textvariable=varsMinute)
+sMinute.pack()
+labelSeconde = Label(fenetre, text="Seconde")
+labelSeconde.pack()
+sSeconde = Spinbox(fenetre, from_=0, to=60, textvariable=varsSeconde)
+sSeconde.pack()
+
+#vitesse
+labelVitesse = Label(fenetre, text="Vitesse")
+labelVitesse.pack()
+sVitesse = Spinbox(fenetre, from_=1.0, to=100000, textvariable=varsVitesse)
+sVitesse.pack()
+
+#boutons start
+boutonStart = Button(fenetre, text="Launch", command=lauchSimulation)
+boutonStart.pack()
+boutonStartRealTime = Button(fenetre, text="Launch with real time", command=lauchSimulationInRealTime)
+boutonStartRealTime.pack()
+
+fenetre.mainloop()
